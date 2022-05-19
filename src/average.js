3c65568101360b7c231c78fb0e1573f24f9a6110
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-05-19 19:10:56 -0300

    ajustes iniciais

--- src/average.js
+++ src/average.js
@@ -1,7 +1,10 @@
 /*
-  A função average recebe um array de tamanho variável e retorna a média dos valores recebidos.
-  Caso a função receba algum valor não numérico ou um array vazio, o valor undefined deve ser retornado.
-  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
+  A função average recebe um array de tamanho variável e retorna a média dos valores 
+  recebidos.
+  Caso a função receba algum valor não numérico ou um array vazio, o valor undefined 
+  deve ser retornado.
+  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 
+  vira 1.
 
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
@@ -11,8 +14,21 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // adicione seu código aqui
+const average = ([array]) => {
+if(array !== Number || average === []) {
+  return undefined;
+} else {
+  for(let i = 0; i < array.length; i += 1) {
+    let sum = 0;
+    sum += array[i];
+  }
+  return Math.array;
 };
+console.log(sum);
+
+
+// como calcular a média de um array de tamanho variável? - 
+// soma dos itens do array (n) dividido pela quantidade de itens do array (array.length)
+// para a soma dos itens devo fazer um for capturando cada elemento e somando entre si
 
 module.exports = average;
