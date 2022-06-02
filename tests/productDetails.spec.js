7952e22913d3a56744eae05cb8ca7630a17a50e5
Fernando J Monteiro <ferjmonteiro@gmail.com>
2022-06-02 09:41:15 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -35,16 +35,30 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
     // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
-    expect(productDetails).toEqual(() => {});
+    expect(typeof productDetails).toBe('function');
     // Teste se o retorno da função é um array.
-    expect(productDetails).toEqual([]);
+    expect(typeof productDetails()).toEqual('object');
     // Teste se o array retornado pela função contém dois itens dentro.
-    const itens = expect(Object.keys(productDetails).length).toBe(2);
-        // Teste se os dois itens dentro do array retornado pela função são objetos.
-    expect(productDetails(itens)).toEqual({});
-        // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    expect(productDetails).
+    expect(Object.keys(productDetails()).length).toBe(2);
+    // Teste se os dois itens dentro do array retornado pela função são objetos.
+    const itens = productDetails()
+    expect(typeof itens[0]).toEqual('object');
+    expect(typeof itens[1]).toEqual('object');
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('firstProduct', 'secondProduct')).toEqual([
+      { name: 'firstProduct', details: { productId: 'firstProduct123' } },
+      { name: 'secondProduct', details: { productId: 'secondProduct123' } }
+    ]);
+    expect(productDetails('secondProduct', 'firstProduct')).toEqual([
+      { name: 'secondProduct', details: { productId: 'secondProduct123' } },
+      { name: 'firstProduct', details: { productId: 'firstProduct123' } },
+    ]);
+
     // Teste se os dois productIds terminam com 123.
-    expect(productDetails.includes(123));
+    const item1 = productDetails()[0].details.productId
+    const item2 = productDetails()[1].details.productId
+    expect(item1.endsWith('123')).toBe(true);
+    expect(item2.endsWith('123')).toBe(true);
+
   });
-});
+})
